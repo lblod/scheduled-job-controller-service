ddda2a70017a129ce6fd5d3dfe2946592b7aee49
David <david.journo.31@gmail.com>
2023-04-26 15:20:12 +0200

    Attach source collection authentication configuration to cloned collection.

--- constants.js
+++ constants.js
@@ -12,21 +12,28 @@ export const FILE_DATA_OBJECT_TYPE = 'http://www.semanticdesktop.org/ontologies/
 export const RDF_PREDICATE = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type';
 export const REPEAT_FREQUENCY_PREDICATE = 'http://schema.org/repeatFrequency';
 
+export const BASIC_AUTH = 'https://www.w3.org/2019/wot/security#BasicSecurityScheme';
+export const OAUTH2 = 'https://www.w3.org/2019/wot/security#OAuth2SecurityScheme';
+
 export const PREFIXES = `
-  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-  PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
-  PREFIX dct: <http://purl.org/dc/terms/>
-  PREFIX prov: <http://www.w3.org/ns/prov#>
-  PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
-  PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-  PREFIX oslc: <http://open-services.net/ns/core#>
-  PREFIX cogs: <http://vocab.deri.ie/cogs#>
-  PREFIX adms: <http://www.w3.org/ns/adms#>
-  PREFIX schema: <http://schema.org/>
-  PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
-  PREFIX hrvst: <http://lblod.data.gift/vocabularies/harvesting/>
-  PREFIX rpioHttp: <http://redpencil.data.gift/vocabularies/http/>
-  PREFIX dgftSec: <http://lblod.data.gift/vocabularies/security/>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX muAccount: <http://mu.semte.ch/vocabularies/account/>
+    PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
+    PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX prov: <http://www.w3.org/ns/prov#>
+    PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX oslc: <http://open-services.net/ns/core#>
+    PREFIX cogs: <http://vocab.deri.ie/cogs#>
+    PREFIX adms: <http://www.w3.org/ns/adms#>
+    PREFIX schema: <http://schema.org/>
+    PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+    PREFIX dgftOauth: <http://kanselarij.vo.data.gift/vocabularies/oauth-2.0-session/>
+    PREFIX hrvst: <http://lblod.data.gift/vocabularies/harvesting/>
+    PREFIX wotSec: <https://www.w3.org/2019/wot/security#>
+    PREFIX rpioHttp: <http://redpencil.data.gift/vocabularies/http/>
+    PREFIX dgftSec: <http://lblod.data.gift/vocabularies/security/>
 `;
 
 export const TASK_URI_PREFIX = 'http://redpencil.data.gift/id/task/';
